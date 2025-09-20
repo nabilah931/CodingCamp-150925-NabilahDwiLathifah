70fd40138e5ba86207ac4070113f6f7aa4960d77
nabilah931 <73892305+nabilah931@users.noreply.github.com>
2025-09-20 10:17:38 +0700

    Implement todo list functionality with input fields and display

---
+++ JS/script.js
@@ -0,0 +1,50 @@
+let todos = [];
+
+function addTodo() {
+    /// Get input values
+    const todoInput = document.getElementById("todo-input");
+    const todoDate = document.getElementById("todo-date");
+
+    if (validateInput(todoInput.value, todoDate.value)) {
+        
+        /// Add to todos array
+        const formTodo = { task: todoInput.value, date: todoDate.value };
+        todos.push(formTodo);
+
+        /// Display updated todos
+        displayTodos();
+
+        /// Clear input fields
+        todoInput.value = "";
+        todoDate.value = "";
+    }
+}
+
+function displayTodos() {
+    /// clear existing list
+    const todoList = document.getElementById("todo-list");
+    todoList.innerHTML = "";
+
+    /// Display all todos
+    todos.forEach((todo, _) => {
+        todoList.innerHTML += `
+            <li class="border-b border-gray-300 py-2">${todo.task} - <span 
+                    class="text-sm text-gray-500">${todo.date}</span></li>
+        `;
+    });
+}
+
+/// Mandate functions for future development
+function deleteTodo() { }
+
+/// Mandate functions for future development
+function filterTodo() { }
+
+function validateInput(todo, date) {
+    /// Simple validation
+    if (todo === "" || date === "") {
+        alert("Please fill in both the todo and date fields.");
+        return false;
+    }
+    return true;
+}
