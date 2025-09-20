0a69e17d5bfba861dddc0ddfa8dde5f5c0f81e01
nabilah931 <73892305+nabilah931@users.noreply.github.com>
2025-09-20 10:57:02 +0700

    Implement delete and filter functionality for todos

--- JS/script.js
+++ JS/script.js
@@ -35,10 +35,23 @@ function displayTodos() {
 }
 
 /// Mandate functions for future development
-function deleteTodo() { }
+function deleteTodo() {
+    const todoInput = document.getElementById("todo-list");
+    todoList.innerHTML = "";
+
+    /// Display all todos
+    todos.forEach((todo, _) => {
+        todoList.innerHTML += `
+            <li class="border-b border-gray-300 py-2">${todo.task} - <span 
+                    class="text-sm text-gray-500">${todo.date}</span></li>
+        `;
+    });
+}
 
 /// Mandate functions for future development
-function filterTodo() { }
+function filterTodo() {
+    console.log("Filter function to be implemented");
+}
 
 function validateInput(todo, date) {
     /// Simple validation
